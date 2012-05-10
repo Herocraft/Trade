517a2fbd027cedfee4aa97ea1813ad43b0ce9e1f
Jos van 't Hof <josvth@live.nl>
2012-05-10 23:11:58 +0200

    Added RequestListener. More accept checks and fixed the acceptRequest() and refuseRequest() functions.

--- Trade/src/me/Josvth/Trade/Trade.java
+++ Trade/src/me/Josvth/Trade/Trade.java
@@ -10,6 +10,7 @@ import me.Josvth.Trade.Handlers.CommandHandler;
 import me.Josvth.Trade.Handlers.TradeHandler;
 import me.Josvth.Trade.Handlers.YamlHandler;
 import me.Josvth.Trade.Listeners.InTradeListener;
+import me.Josvth.Trade.Listeners.RequestListener;
 
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,6 +31,7 @@ public class Trade extends JavaPlugin{
 	CommandHandler commandHandler;
 	
 	InTradeListener inTradeListener;
+	RequestListener requestListener;
 	
 	@Override
 	public void onEnable() {
@@ -40,7 +42,7 @@ public class Trade extends JavaPlugin{
 		commandHandler = new CommandHandler(this);
 		
 		inTradeListener = new InTradeListener(this);
-		
+		requestListener = new RequestListener(this);
 	}
 	
 	public void requestPlayer(final Player requester, final Player requested){
@@ -48,10 +50,24 @@ public class Trade extends JavaPlugin{
 			languageHandler.sendMessage(requester, Message.REQUEST_NO_PERMISSION, "", "", "");
 			return;
 		}
+		if(!requester.canSee(requested) && yamlHandler.configYaml.getBoolean("must_see",true)){
+			languageHandler.sendMessage(requester, Message.REQUEST_MUST_SEE, "", "", "");
+			return;
+		}
+		if(!requester.getWorld().equals(requested.getWorld()) && !yamlHandler.configYaml.getBoolean("cross_world",false)){
+			languageHandler.sendMessage(requester, Message.REQUEST_NO_CROSS_WORLD, "", "", "");
+			return;
+		}
+		if(!requester.getGameMode().equals(requested.getGameMode()) && !yamlHandler.configYaml.getBoolean("cross_gamemode",false)){
+			languageHandler.sendMessage(requester, Message.REQUEST_NO_CROSS_GAMEMODE, "", "", "");
+			return;
+		}
+		
 		if(activeTrades.containsKey(requested)){
 			languageHandler.sendMessage(requester, Message.REQUEST_IN_TRADE, requested.getName(), "", "");
 			return;
 		}
+		
 		if(ignoring.contains(requested)){
 			languageHandler.sendMessage(requester, Message.REQUEST_IGNORING, requested.getName(), "", "");
 			return;
@@ -66,22 +82,22 @@ public class Trade extends JavaPlugin{
 		getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
 			@Override
 			public void run() {
-				if(pendingRequests.containsKey(requested)){
+				if(pendingRequests.containsKey(requester)){
 					languageHandler.sendMessage(requester, Message.REQUEST_NO_RESPONSE, requested.getName(), "", "");
-					pendingRequests.remove(requested);
+					pendingRequests.remove(requester);
 				}
 			}
 		}, yamlHandler.configYaml.getInt("request_timeout",10)*20);
 	}
 	
-	public void acceptRequest(Player requested, Player requestor){	
-		startTrading(requested, requestor);
-		pendingRequests.remove(requestor);
+	public void acceptRequest(Player requested, Player requester){	
+		startTrading(requested, requester);
+		pendingRequests.remove(requester);
 	}
 	
-	public void refuseRequest(Player requested, Player requestor){
-		languageHandler.sendMessage(requestor, Message.REQUEST_REFUSED, requested.getName(), "", "");
-		pendingRequests.remove(requestor);
+	public void refuseRequest(Player requested, Player requester){
+		languageHandler.sendMessage(requester, Message.REQUEST_REFUSED, requested.getName(), "", "");
+		pendingRequests.remove(requester);
 	}	
 		
 	public void startTrading(Player player1, Player player2){
