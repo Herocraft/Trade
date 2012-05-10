3cca0d347f3a6360db428ef1ad08387fb150db58
Jos van 't Hof <josvth@live.nl>
2012-05-10 23:13:11 +0200

    Added dynamic TradeInventory sizes methods.

--- Trade/src/me/Josvth/Trade/Handlers/TradeHandler.java
+++ Trade/src/me/Josvth/Trade/Handlers/TradeHandler.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import me.Josvth.Trade.Trade;
 import me.Josvth.Trade.Exeptions.PlayerNotFoundExeption;
 import me.Josvth.Trade.Handlers.LanguageHandler.Message;
+import me.Josvth.Trade.TradingInventories.StandardInventory;
 import me.Josvth.Trade.TradingInventories.TradingInventory;
 
 import org.bukkit.entity.Player;
@@ -16,7 +17,7 @@ public class TradeHandler implements Runnable {
 
 	Player p1, p2;
 	TradingInventory p1inv, p2inv;
-	boolean p1acc, p2acc;
+	boolean p1acc = false, p2acc = false;
 
 	public TradeHandler(Trade instance, Player player1, Player player2) {
 		plugin = instance;
@@ -26,7 +27,29 @@ public class TradeHandler implements Runnable {
 	}
 
 	private void createTradingInventories() {
-
+		
+		int rows = 4;
+		int rowsP1 = getAllowedGuiRows(p1);
+		int rowsP2 = getAllowedGuiRows(p2);
+		
+		// Yes this can be done with a xor but I'm lazy
+		
+		if(rowsP2 > rowsP1){
+			if(plugin.yamlHandler.configYaml.getBoolean("use_biggest", true)){
+				rows = rowsP2;
+			}else{
+				rows = rowsP1;
+			}
+		}else{
+			if(plugin.yamlHandler.configYaml.getBoolean("use_biggest", true)){
+				rows = rowsP1;
+			}else{
+				rows = rowsP2;
+			}
+		}
+		
+		p1inv = new StandardInventory(plugin, p2.getName(),	rows);
+		p2inv = new StandardInventory(plugin, p1.getName(), rows);
 	}
 
 	public void acceptTrade(Player player) throws PlayerNotFoundExeption{
@@ -40,6 +63,7 @@ public class TradeHandler implements Runnable {
 		plugin.languageHandler.sendMessage(player, Message.TRADE_ACCEPT_SELF, "", "", "");
 		plugin.languageHandler.sendMessage(getOtherPlayer(player), Message.TRADE_ACCEPT_OTHER, player.getName(), "", "");
 		getTradingInventory(player).accept();
+		getTradingInventory(getOtherPlayer(player)).acceptOther();
 		if(p1acc && p2acc){
 			finishTrade();
 		}
@@ -56,6 +80,7 @@ public class TradeHandler implements Runnable {
 		plugin.languageHandler.sendMessage(player, Message.TRADE_DENY_SELF, "", "", "");
 		plugin.languageHandler.sendMessage(getOtherPlayer(player), Message.TRADE_DENY_OTHER, player.getName(), "", "");
 		getTradingInventory(player).deny();
+		getTradingInventory(getOtherPlayer(player)).denyOther();
 	}
 
 	private void cancelAccept(Player player) throws PlayerNotFoundExeption{
@@ -71,11 +96,12 @@ public class TradeHandler implements Runnable {
 	}
 
 	public void refuse(Player player) throws PlayerNotFoundExeption{
-		if(player.equals(p1) || player.equals(p2)){
+		if(!player.equals(p1) && !player.equals(p2)){
 			throw new PlayerNotFoundExeption();
 		}else{
 			plugin.languageHandler.sendMessage(player, Message.TRADE_REFUSE_SELF, "", "", "");
 			plugin.languageHandler.sendMessage(getOtherPlayer(player), Message.TRADE_REFUSE_OTHER, getOtherPlayer(player).getName(), "", "");
+			stopTrading();
 		}
 	}
 	
@@ -116,35 +142,48 @@ public class TradeHandler implements Runnable {
 		plugin.languageHandler.sendMessage(p2, Message.TRADE_START, p1.getName(), "", "");
 		plugin.languageHandler.sendMessage(p1, Message.TRADE_HELP, p2.getName(), p1inv.getAcceptItem().getType().name(), p1inv.getRefuseItem().getType().name());
 		plugin.languageHandler.sendMessage(p2, Message.TRADE_HELP, p1.getName(), p2inv.getAcceptItem().getType().name(), p1inv.getRefuseItem().getType().name());
+		plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, 5);
 	}
 
 	private void finishTrade() {
 		giveItems();
 		plugin.activeTrades.remove(p1);
-		plugin.activeTrades.remove(p2);	
+		plugin.activeTrades.remove(p2);
+		p1.closeInventory();
+		p2.closeInventory();
 	}
 	
 	public void stopTrading() {
 		revertItems();
 		plugin.activeTrades.remove(p1);
-		plugin.activeTrades.remove(p2);		
+		plugin.activeTrades.remove(p2);
+		p1.closeInventory();
+		p2.closeInventory();
 	}
 
 	private void giveItems() {
 		for(ItemStack item : p1inv.getPendingItems()){
-			p1.getInventory().addItem(item);
+			if(item != null){
+				p1.getInventory().addItem(item);
+			}
 		}
 		for(ItemStack item : p2inv.getPendingItems()){
-			p2.getInventory().addItem(item);
+			if(item != null){
+				p2.getInventory().addItem(item);
+			}
 		}
 	}
 	
 	private void revertItems() {
 		for(ItemStack item : p1inv.getOfferedItems()){
-			p1.getInventory().addItem(item);
+			if(item != null){
+				p1.getInventory().addItem(item);
+			}
 		}
 		for(ItemStack item : p2inv.getOfferedItems()){
-			p2.getInventory().addItem(item);
+			if(item != null){
+				p2.getInventory().addItem(item);
+			}
 		}
 	}
 
@@ -177,6 +216,22 @@ public class TradeHandler implements Runnable {
 					e.printStackTrace();
 				}
 			}
+		}
+	}
+	
+	public int getAllowedGuiRows(Player player){
+		if(player.hasPermission("trade.gui.rows.6")){
+			return 6;
+		}else if(player.hasPermission("trade.gui.rows.5")){
+			return 5;
+		}else if(player.hasPermission("trade.gui.rows.4")){
+			return 4;
+		}else if(player.hasPermission("trade.gui.rows.3")){
+			return 3;
+		}else if(player.hasPermission("trade.gui.rows.2")){
+			return 2;
+		}else{
+			return plugin.yamlHandler.configYaml.getInt("default_rows", 4);
 		}
 	}
 }
