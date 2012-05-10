7ad1e117b2fd1de45dddd09e71df0fd506c14fb9
Jos van 't Hof <josvth@live.nl>
2012-05-10 23:12:25 +0200

    Added more messages.

--- Trade/src/me/Josvth/Trade/Handlers/LanguageHandler.java
+++ Trade/src/me/Josvth/Trade/Handlers/LanguageHandler.java
@@ -10,29 +10,46 @@ public class LanguageHandler {
 	Trade plugin;
 
 	public enum Message{
-
-		TRADE_ACCEPT_SELF ("trade.accept.self"),
-		TRADE_ACCEPT_OTHER ("trade.accept.other"), 
-		TRADE_DENY_SELF ("trade.deny.self"),
-		TRADE_DENY_OTHER ("trade.deny.other"),
-		TRADE_OFFER_CHANGED ("trade.offer_changed"),
+		
 		TRADE_START ("trade.start"),
 		TRADE_HELP ("trade.help"),
-		TRADE_CANNOT_USE_SLOT ("trade.cannot_use_slot"),
+		
+		TRADE_ACCEPT_SELF ("trade.accept.self"),
+		TRADE_ACCEPT_OTHER ("trade.accept.other"), 
+		
+		TRADE_DENY_SELF ("trade.deny.self"),
+		TRADE_DENY_OTHER ("trade.deny.other"),
+		
 		TRADE_REFUSE_OTHER ("trade.refuse.other"),
 		TRADE_REFUSE_SELF ("trade.refuse.self"),
 		
+		TRADE_OFFER_CHANGED ("trade.offer_changed"),
+				
+		TRADE_CANNOT_USE_SLOT ("trade.cannot_use_slot"),
+		
+		
+		REQUEST_NEW_SELF ("request.new.self"),
+		REQUEST_NEW_OTHER ("request.new.other"),
+		
+		REQUEST_REFUSED ("request.refused"),
+		
 		REQUEST_IN_TRADE ("request.in_trade"), 
+		
 		REQUEST_IGNORING ("request.ignoring.is_ignoring"),
 		REQUEST_IGNORING_ENABLE ("request.ignoring.enabled"),
 		REQUEST_IGNORING_DISABLE ("request.ignoring.disabled"),
+		
 		REQUEST_PLEASE_WAIT ("request.please_wait"), 
+		
 		REQUEST_NO_PERMISSION ("request.no_permission"), 
-		REQUEST_NEW_SELF ("request.new.self"),
-		REQUEST_NEW_OTHER ("request.new.other"), 
-		REQUEST_NO_RESPONSE ("request.no_response"), 
-		REQUEST_REFUSED ("request.refused"), 
+		REQUEST_NO_CROSS_WORLD ("request.no_cross_world"),
+		REQUEST_NO_CROSS_GAMEMODE ("request.no_cross_gamemode"),
+		REQUEST_MUST_SEE ("request.must_see"),
+		
+		REQUEST_NO_RESPONSE ("request.no_response"),
+				
 		REQUEST_PLAYER_NOT_REQUESTED ("request.player_not_requested"),
+		
 		REQUEST_PLAYER_NOT_FOUND ("request.player_not_found"); 
 		
 
@@ -50,9 +67,9 @@ public class LanguageHandler {
 	public void sendMessage(Player player, Message message, String playerName, String acceptItemName, String refuseItemName) {
 		String string = plugin.yamlHandler.languageYaml.getString(message.path);
 		if(string != null){
-			string.replaceAll("&other_player", playerName);
-			string.replaceAll("&accept_item", acceptItemName);
-			string.replaceAll("&refuse_item", refuseItemName);
+			string = string.replaceAll("&other_player", playerName);
+			string = string.replaceAll("&accept_item", acceptItemName);
+			string = string.replaceAll("&refuse_item", refuseItemName);
 			player.sendMessage(formatColorCodes(string));
 		}
 	}
