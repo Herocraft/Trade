1c5aff0e6c039ae4b7d779ff23b51f5c35145a1f
Jos van 't Hof <josvth@live.nl>
2012-05-10 12:39:11 +0200

    Some changes in the rewrite.

--- Trade/src/me/Josvth/Trade/Handlers/TradeHandler.java
+++ Trade/src/me/Josvth/Trade/Handlers/TradeHandler.java
@@ -1,276 +1,89 @@
 package me.Josvth.Trade.Handlers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import me.Josvth.Trade.Trade;
-import me.Josvth.Trade.Inventories.TradeInventory;
+import me.Josvth.Trade.Exeptions.PlayerNotFoundExeption;
+import me.Josvth.Trade.Handlers.LanguageHandler.Message;
+import me.Josvth.Trade.TradingInventories.TradingInventory;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-public class TradeHandler implements Runnable{
+public class TradeHandler {
 
 	Trade plugin;
 
-	int tradeID;
+	Player p1, p2;
+	TradingInventory p1inv, p2inv;
+	boolean p1acc, p2acc;
 
-	Player p1;
-	Player p2;
-
-	TradeInventory p1TradeInventory;
-	TradeInventory p2TradeInventory;
-
-	boolean p1accept = false;
-	boolean p2accept = false;
-
-	public TradeHandler(Trade instance, Player player1, Player player2){
-
+	public TradeHandler(Trade instance, Player player1, Player player2) {
 		plugin = instance;
-
 		p1 = player1;
 		p2 = player2;
-
-		int rowsP1 = getAllowedGuiRows(p1);
-		int rowsP2 = getAllowedGuiRows(p2);
-
-		int rows;
-
-		if(rowsP1 > rowsP2 ^ plugin.config.getBoolean("use_biggest",true)){
-			rows = rowsP1;
-		}else{
-			rows = rowsP2;
-		}
-
-		p1TradeInventory = new TradeInventory(instance, p1.getName(), p2.getName(), rows);
-		p2TradeInventory = new TradeInventory(instance, p2.getName(), p1.getName(), rows);
-
+		createTradingInventories();
 	}
 
-	private int getAllowedGuiRows(Player player){
-		if(player.hasPermission("trade.gui.rows.6")){
-			return 6;
-		}else if(player.hasPermission("trade.gui.rows.5")){
-			return 5;
-		}else if(player.hasPermission("trade.gui.rows.4")){
-			return 4;
-		}else if(player.hasPermission("trade.gui.rows.3")){
-			return 3;
-		}else if(player.hasPermission("trade.gui.rows.2")){
-			return 2;
-		}else{
-			return plugin.config.getInt("default_gui_rows",4);
-		}
+	private void createTradingInventories() {
+
 	}
-
-	public void startTrading(){
-
-		tradeID = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, 10);
-
-		p1.openInventory(p1TradeInventory.getInventory());
-		p2.openInventory(p2TradeInventory.getInventory());
-
-		if(plugin.config.getString("messages.trade_start") != null){
-			p1.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.trade_start").replaceAll("&other_player", p2.getName())));
-			p2.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.trade_start").replaceAll("&other_player", p1.getName())));
-		}
-
-		if(plugin.config.getString("messages.trade_help") != null){
-			String help = Trade.formatChatColors(plugin.config.getString("messages.trade_help").replaceAll("&accept_item",p1TradeInventory.getAcceptItem().getType().name().toLowerCase()).replaceAll("&refuse_item", p1TradeInventory.getRefuseItem().getType().name().toLowerCase()));
-			p1.sendMessage(help);
-			p2.sendMessage(help);
-		}
-	}
-
-	public void accept(Player player) {
-
+	
+	public void acceptTrade(Player player) throws PlayerNotFoundExeption{
 		if(player.equals(p1)){
-			p1accept = true;
+			p1acc = true;
+		}else if(player.equals(p2)){
+			p2acc = true;
 		}else{
-			p2accept = true;
+			throw new PlayerNotFoundExeption();
 		}
-
-		if(plugin.config.getString("messages.accept") != null){
-			player.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.accept")));
-		}
-
-		if(plugin.config.getString("messages.accept_other") != null){
-			getOtherPlayer(player).sendMessage(Trade.formatChatColors(plugin.config.getString("messages.accept_other").replaceAll("&other_player", player.getName())));
-		}
-
-		getTradeInventory(player).setPending();
-
-		if(p1accept && p2accept) finishTrading();
-
+		plugin.languageHandler.sendMessage(player, Message.TRADE_ACCEPT_SELF, "", "", "");
+		plugin.languageHandler.sendMessage(getOtherPlayer(player), Message.TRADE_ACCEPT_OTHER, player.getName(), "", "");
+		getTradingInventory(player).accept();
 	}
-
-	public void deaccept(Player player) {
-
+	
+	public void denyTrade(Player player) throws PlayerNotFoundExeption{
 		if(player.equals(p1)){
-			p1accept = false;
+			p1acc = false;
+		}else if(player.equals(p2)){ 
+			p2acc = false;	
 		}else{
-			p2accept = false;
+			throw new PlayerNotFoundExeption();
 		}
-
-		if(plugin.config.getString("messages.deaccept") != null){
-			player.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.deaccept")));
-		}
-
-		if(plugin.config.getString("messages.deaccept_other") != null){
-			getOtherPlayer(player).sendMessage(Trade.formatChatColors(plugin.config.getString("messages.deaccept_other").replaceAll("&other_player", player.getName())));
-		}
-
-		getTradeInventory(player).resetPending();
+		plugin.languageHandler.sendMessage(player, Message.TRADE_DENY_SELF, "", "", "");
+		plugin.languageHandler.sendMessage(getOtherPlayer(player), Message.TRADE_DENY_OTHER, player.getName(), "", "");
+		getTradingInventory(player).deny();
 	}
-
-	public void cancelAccept(Player player){
-
+	
+	private Player getOtherPlayer(Player player) throws PlayerNotFoundExeption {
 		if(player.equals(p1)){
-			p1accept = false;
+			return p2;
+		}else if(player.equals(p2)){
+			return p1;
 		}else{
-			p2accept = false;
-		}
-
-		if(plugin.config.getString("messages.offer_changed") != null){
-			player.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.offer_changed").replaceAll("&other_player", getOtherPlayer(player).getName())));
-		}
-
-		getTradeInventory(player).resetPending();
-	}
-
-	public void refuse(Player player){
-		if(plugin.config.getString("messages.refuse") != null){
-			player.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.refuse")));
-		}
-
-		if(plugin.config.getString("messages.refuse_other") != null){
-			getOtherPlayer(player).sendMessage(Trade.formatChatColors(plugin.config.getString("messages.refuse_other").replaceAll("&other_player", player.getName())));
-		}
-
-		stopTrading();
-	}
-
-	public void abandon(Player player) {
-		if(plugin.config.getString("messages.abandon") != null){
-			player.sendMessage(Trade.formatChatColors(plugin.config.getString("messages.abandon")));
-		}
-		if(plugin.config.getString("messages.abandon_other") != null){
-			getOtherPlayer(player).sendMessage(Trade.formatChatColors(plugin.config.getString("messages.abandon_other").replaceAll("&other_player", player.getName())));
-		}
-
-		stopTrading();
-	}
-
-	// Inventories matcher
-	@Override
-	public void run() {
-
-		ItemStack[] p1OfferedItems = getOfferedItems(p1);
-		ItemStack[] p2PendingItems = getPendingItemsAt(p2);
-
-		if(!Arrays.equals(p1OfferedItems, p2PendingItems)){
-			updateOfferedItems(p1,p2);
-			if(p2accept) cancelAccept(p2);
-		}
-
-		ItemStack[] p2OfferedItems = getOfferedItems(p2);
-		ItemStack[] p1PendingItems = getPendingItemsAt(p1);
-
-		if(!Arrays.equals(p2OfferedItems, p1PendingItems)){
-			updateOfferedItems(p2,p1);
-			if(p1accept) cancelAccept(p1);
+			throw new PlayerNotFoundExeption();
 		}
 	}
 
-	private void updateOfferedItems(Player p1, Player p2) {
-		setPendingItems(p2, getOfferedItems(p1));		
+	private TradingInventory getTradingInventory(Player player){
+		if(player.equals(p1)) return p1inv;
+		if(player.equals(p2)) return p2inv;
+		return null;
 	}
 
-	private void setPendingItems(Player player, ItemStack[] offeredItems) {
-		for(int slot = 0; slot < offeredItems.length; slot++){
-			getTradeInventory(player).getInventory().setItem(getTradeInventory(player).getRightSlots().get(slot), offeredItems[slot]);
-		}		
+	public void startTrading() {
+
 	}
 
-	private ItemStack[] getPendingItemsAt(Player player) {
-		List<ItemStack> pendingItems = new ArrayList<ItemStack>();
-
-		for(int slot : getTradeInventory(player).getLeftSlots()){
-			pendingItems.add(getTradeInventory(player).getInventory().getItem(slot));
-		}
-
-		ItemStack[] pendingItemsList = new ItemStack[pendingItems.size()];
-
-		return pendingItems.toArray(pendingItemsList);
-	}
-
-	private ItemStack[] getOfferedItems(Player player) {
-		List<ItemStack> offeredItems = new ArrayList<ItemStack>();
-
-		for(int slot: getTradeInventory(player).getLeftSlots()){
-			offeredItems.add(getTradeInventory(player).getInventory().getItem(slot));
-		}
-
-		ItemStack[] offeredItemsList = new ItemStack[offeredItems.size()];
-
-		return offeredItems.toArray(offeredItemsList);
-	}
-
-	// ---------
-
-	public void finishTrading(){
-		for(int slot : getTradeInventory(p1).getRightSlots()){
-			if(p1TradeInventory.getInventory().getItem(slot) != null)  p1.getInventory().addItem(p1TradeInventory.getInventory().getItem(slot));
-		}
-
-		for(int slot : getTradeInventory(p2).getRightSlots()){
-			if(p2TradeInventory.getInventory().getItem(slot) != null)  p2.getInventory().addItem(p2TradeInventory.getInventory().getItem(slot));
-		}
-
-		p1.closeInventory();
-		p2.closeInventory();
-
+	public void stopTrading() {
+		revertItems();
 		plugin.activeTrades.remove(p1);
-		plugin.activeTrades.remove(p2);
-
-		plugin.getServer().getScheduler().cancelTask(tradeID);
-
+		plugin.activeTrades.remove(p2);		
 	}
 
-	public void stopTrading(){
+	private void revertItems() {
+		for(int slot : p1inv.getOwnSlots()){
 
-		for(int slot : getTradeInventory(p1).getLeftSlots()){
-			if(p1TradeInventory.getInventory().getItem(slot) != null)  p1.getInventory().addItem(p1TradeInventory.getInventory().getItem(slot));
 		}
+		for(int slot : p2inv.getOwnSlots()){
 
-		for(int slot : getTradeInventory(p2).getLeftSlots()){
-			if(p2TradeInventory.getInventory().getItem(slot) != null)  p2.getInventory().addItem(p2TradeInventory.getInventory().getItem(slot));
-		}
-
-		p1.closeInventory();
-		p2.closeInventory();
-
-		plugin.activeTrades.remove(p1);
-		plugin.activeTrades.remove(p2);
-
-		plugin.getServer().getScheduler().cancelTask(tradeID);
-
-	}
-
-	public TradeInventory getTradeInventory(Player player) {
-		return player.equals(p1) ? p1TradeInventory : p2TradeInventory;
-	}
-
-	private Player getOtherPlayer(Player player) {
-		return player.equals(p1) ? p2 : p1;
-	}
-
-	public boolean hasAccepted(Player player) {
-		if(player.equals(p1)){
-			return p1accept;
-		}else{
-			return p2accept;
 		}
 	}
 }
--- Trade/src/me/Josvth/Trade/Trade.java
+++ Trade/src/me/Josvth/Trade/Trade.java
@@ -2,103 +2,76 @@ package me.Josvth.Trade;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
+import me.Josvth.Trade.Handlers.LanguageHandler;
+import me.Josvth.Trade.Handlers.LanguageHandler.Message;
 import me.Josvth.Trade.Handlers.CommandHandler;
 import me.Josvth.Trade.Handlers.TradeHandler;
 import me.Josvth.Trade.Handlers.YamlHandler;
-import me.Josvth.Trade.Listeners.InTradeListener;
-import me.Josvth.Trade.Listeners.RequestListener;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Trade extends JavaPlugin{
 
 	Logger logger;
-
-	public HashMap<Player,Player> pendingRequests = new HashMap<Player,Player>();
-	public Map<Player,TradeHandler> activeTrades = new HashMap<Player,TradeHandler>();
-	public List<Player> ignoreRequests = new ArrayList<Player>();
-
-	InTradeListener inTradeListener;
-	RequestListener requestListener;
-
-	CommandHandler commandHandler;
-	public YamlHandler yamlHandler;
-
-	@Override
-	public void onDisable(){
-	}
 	
-	//test commit in branch
+	// key = requester , value = requested
+	HashMap<Player,Player> pendingRequests = new HashMap<Player, Player>();
+	
+	public HashMap<Player,Object> activeTrades = new HashMap<Player, Object>();
+
+	ArrayList<Player> ignoring = new ArrayList<Player>();
+	
+	public YamlHandler yamlHandler;
+	public LanguageHandler languageHandler;
+	CommandHandler commandHandler;
 	
 	@Override
 	public void onEnable() {
-
 		logger = getLogger();
-
-		commandHandler = new CommandHandler(this);
-		yamlHandler = new YamlHandler(this);
-
-		inTradeListener = new InTradeListener(this);
-		requestListener = new RequestListener(this);
-
 	}
-
-	public void request(final Player requestor, final Player requested){
-
-		if(ignoreRequests.contains(requested) || requested.hasPermission("trade.ignorerequests")){
-			if(yamlHandler.messagesYaml.isString("request.ignoring")){
-				requestor.sendMessage(formatChatColors(yamlHandler.messagesYaml.getString("request.ignoring").replaceAll("&other_player", requested.getName())));
-			}
-			return;
-		}	
-
-		if(!requestor.getWorld().equals(requestor.getWorld()) && !yamlHandler.messagesYaml.getBoolean("crossworld_trading", false)){
-			if(yamlHandler.messagesYaml.isString("request.ignoring")){
-				requestor.sendMessage(formatChatColors(yamlHandler.messagesYaml.getString("request.no_crossworld").replaceAll("&other_player", requested.getName())));
-			}
+	
+	public void requestPlayer(final Player requester, final Player requested){
+		if(!requester.hasPermission("trade.request")){
+			languageHandler.sendMessage(requester, Message.REQUEST_NO_PERMISSION, "", "", "");
 			return;
 		}
-		{
-
-			if(yamlHandler.messagesYaml.isString("request")){
-				requested.sendMessage(formatChatColors(yamlHandler.messagesYaml.getString("request.other").replaceAll("&other_player", requestor.getName())));
-			}
-
-			if(yamlHandler.messagesYaml.isString("request_self")){
-				requestor.sendMessage(formatChatColors(yamlHandler.messagesYaml.getString("request.self").replaceAll("&other_player", requested.getName())));
-			}
-
-			getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
-				public void run() {	
-					if(pendingRequests.containsKey(requestor)){
-						requestor.sendMessage(formatChatColors(yamlHandler.messagesYaml.getString("request.no_response").replaceAll("&other_player", requested.getName())));
-						pendingRequests.remove(requestor);
-					}
-				}
-			}, yamlHandler.configYaml.getInt("request_timeout",10) * 20);
-
-			pendingRequests.put(requestor, requested);
+		if(activeTrades.containsKey(requested)){
+			languageHandler.sendMessage(requester, Message.REQUEST_IN_TRADE, requested.getName(), "", "");
+			return;
 		}
+		if(ignoring.contains(requested)){
+			languageHandler.sendMessage(requester, Message.REQUEST_IGNORING, requested.getName(), "", "");
+			return;
+		}
+		if(pendingRequests.containsKey(requester)){
+			languageHandler.sendMessage(requester, Message.REQUEST_PLEASE_WAIT, requested.getName(), "", "");
+			return;
+		}
+		pendingRequests.put(requester, requested);
+		languageHandler.sendMessage(requester, Message.REQUEST_NEW_SELF, requested.getName(), "", "");
+		languageHandler.sendMessage(requested, Message.REQUEST_NEW_OTHER, requester.getName(), "", "");
+		getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
+			@Override
+			public void run() {
+				languageHandler.sendMessage(requester, Message.REQUEST_NO_RESPONSE, requested.getName(), "", "");
+				pendingRequests.remove(requested);
+			}
+		}, yamlHandler.config.getInt("request_timeout",10)*20);
 	}
-
-	public void startNewTrade(Player player1, Player player2) {
+	
+	public void cancelRequest(Player requester){
+		pendingRequests.remove(requester);
+	}
+	
+	public void startTrading(Player player1, Player player2){
 		TradeHandler tradeHandler = new TradeHandler(this, player1, player2);
 		activeTrades.put(player1, tradeHandler);
 		activeTrades.put(player2, tradeHandler);
 		tradeHandler.startTrading();
 	}
 
-	public static String formatChatColors(String string){
-		return ChatColor.translateAlternateColorCodes('&', string);
-	}
 
-	public void logWarning(String message){
-		logger.warning(message);
-	}
 }
